33a3b079aaa1d6a2ec8ad22d48ad9029f3c54261
Ivan Yavorin <Yavorin@ukr.net>
2020-12-04 21:42:50 +0200

    adding empty project

---
+++ MVVM-RxSwift-Test/MVVM-RxSwift-Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-RxSwift-Test
+//
+//  Created by Ivan Yavorin on 04.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
